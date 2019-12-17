7ce4e23a89b99f9c767efadbd83c29fbe94f9a25
abprakash405 <abprakash405@gmail.com>
2019-12-17 23:14:14 +0530

    Update Pipeline.groovy

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -2,15 +2,15 @@
 
 def call(body) {
     def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+    //body.resolveStrategy = Closure.DELEGATE_FIRST
+    //body.delegate = config
+    //body()
 
     echo config.name
-    echo "Param1 is: ${env.param1}"
-    echo "Param2 is: ${env.param2}"
-    if (env.param1 == 'One default') {
-        echo "Param1 is default"
-    }
+    //echo "Param1 is: ${env.param1}"
+    //echo "Param2 is: ${env.param2}"
+    //if (env.param1 == 'One default') {
+//        echo "Param1 is default"
+  //  }
     return this
 }

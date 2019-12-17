0e0259ff0de8aabb59c6951f19afc3b1c70e9b28
abprakash405 <abprakash405@gmail.com>
2019-12-17 23:27:51 +0530

    Update Pipeline.groovy

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
-
+import src.Sample
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    def sample = new Sample()
+    
     Sample()
     echo config.name
     

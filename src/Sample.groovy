fadb36ea8cb58cc1d01500b312d9bdd299e4f18b
abprakash405 <abprakash405@gmail.com>
2020-07-27 19:18:59 +0530

    Update Sample.groovy

--- src/Sample.groovy
+++ src/Sample.groovy
@@ -2,9 +2,7 @@ public class Sample {
 	int a = 1
 	int b = 2
 	Script script 
-	def steps 
 	def run() {
 		script.echo("Cloud DevOps training course from AspireNew Technologies")
-		steps.sh "echo 'hello' "
 	}
 }
